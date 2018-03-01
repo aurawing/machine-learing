bd32dc47b5778c58e22d85b9559cf13ca2d465a9
aurawing <zhangxusmall@163.com>
2018-03-01 14:45:43 +0800

    exerice 2: Logistic Regression Cost and Gradient

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,10 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+J = (y' * log(sigmoid(X * theta)) + (1 - y') * log(1 - sigmoid(X * theta))) * (-1) / m;
 
-
-
-
+grad = X' * (sigmoid(X * theta) - y) / m;
 
 
 
