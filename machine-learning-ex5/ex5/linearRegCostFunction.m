0f0fab3db76253f6411a3550f64c461df16892a3
aurawing <zhangxusmall@163.com>
2018-03-09 17:20:06 +0800

    exercise 5: earning curves

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,9 +18,9 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+J = sum((X * theta - y) .^ 2) / m / 2 + sum(theta(2:end) .^ 2) * lambda / m / 2;
 
-
-
+grad = X' * (X * theta - y) / m + [0; theta(2:end)] * lambda / m;
 
 
 
